36a17aadce4e3cd80f59e4e5eb091539a78dfdd2
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-18 20:01:02 +1000

    Seed data done. Product MVC and CRUD done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+
+Product.create(:title=>"Product 1", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
+
+Product.create(:title=>"Product 2", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
+
+Product.create(:title=>"Product 3", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
+
+Product.create(:title=>"Product 4", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
+
+Product.create(:title=>"Product 5", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
