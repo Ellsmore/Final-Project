5ba1160d26377e15e7f2cdc8871b002662381f3b
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-19 16:11:36 +1000

    Product validation done. Product tests in progress

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image, :format => {
+    :with => %r{\.(gif|jpg|png)\z}i,
+    :message => 'Image must be a URL for a gif, jpg or png image.'
+  }
+
+
+  # Above validations check the fields are not empty, price is a valid number and not less than $0.01, product titles are unqiue and image url follows format
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Ivalid? checks to see whether it validates, any? method sees whether there is an error associated with a particular attribute. Assert is a method call that tells the framework what we expect to be true.
+
+  # assert_equal: optional fail message may be provided as final argument
+
+test 'Product attributes must NOT be empty' do
+  product = Product.new
+
+  assert product.invalid?
+
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:image].any?
+  assert product.errors[:image].any?
+end
+
+test "product price must be positive" do
+  product = Product.new(:title => "Test title", :description => "test description", :image => "test.jpg")
+
+  product.price = -1
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01",
+  product.errors[:price].join('; ')
+
+  product.price = 0
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+  product.price = 1
+  assert product.valid?
+end
+
 end
