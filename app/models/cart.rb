0337b1e5481b025d15fefeeb2d7a2aba79ff0708
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-20 15:30:12 +1000

    Cart product quantities

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy # ':dependent => :destroy' indicates that the existence of line items is dependent on the existence of the cart i.e. When we delete a cart from the DB we want to delete any associated line items also
+
+  def add_product(product_id)
+    current_item = line_items.find_by_product_id(product_id)
+
+    # find_by_product_id is a dynamic finder contstructed by Active Record
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(:product_id => product_id)
+    end
+  current_item
+  end
 end
