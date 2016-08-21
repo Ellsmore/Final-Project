eab9fcfeee277e22ef376b1c90422efeb84d59ee
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-21 20:29:27 +1000

    Order CRUD done. Pagination gem added for Orders page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
+
   get 'store/index'
 
-  root :to => 'store#index', :as => 'store' #create store_path variable 
-  resources :products
+  root :to => 'store#index', :as => 'store' #create store_path variable
+
+  resources :products do
+    get :who_bought, :on => :member
+  end
+  
 end
