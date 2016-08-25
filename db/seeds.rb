cde3534359d31ad60729eed17fdcc02ef0cfd181
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-25 15:12:46 +1000

    stripe payment added

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Product.create(:title=>"Product 2", :description=>"Lorem ipsum dolor sit amet, c
 
 Product.create(:title=>"Product 3", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
 
-Product.create(:title=>"Product 4", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
+Product.create(:title=>"Product 4", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"0.05", :image=>"test.jpg")
 
 Product.create(:title=>"Product 5", :description=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", :price=>"12.95", :image=>"test.jpg")
 
