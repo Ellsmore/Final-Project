36dc96f00a2d5ae98f4951589235e12ab1cafe34
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-08-25 12:28:02 +1000

    Authorisation bug fixed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,7 +24,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(store_url) }
+        format.html { redirect_to(products_url) }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
